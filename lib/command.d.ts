85899710d3f98183f450fc391f982b939560ff41
konojunya <junya03dance@gmail.com>
2021-10-08 20:37:58 +0900

    clean: remove ts

--- lib/command.d.ts
+++
@@ -1,6 +0,0 @@
-declare type Args = [string, string];
-interface Options {
-    token?: string;
-}
-export declare function action(args: Args, options: Options): Promise<void>;
-export {};
--- lib/oauth.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function getTokenFromOAuth(): Promise<string>;
